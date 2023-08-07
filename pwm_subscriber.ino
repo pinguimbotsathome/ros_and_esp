86e930f4fc1e3e4592997f723c8e5c61ccb593d4
Herick DallAgnol <140270456+HerickDallagnol@users.noreply.github.com>
2023-08-07 14:22:49 -0300

    Update pwm_subscriber.ino

--- pwm_subscriber.ino
+++ pwm_subscriber.ino
@@ -1,38 +1,33 @@
-//Codigo na esp
-
 #include <ros.h>
 #include <std_msgs/Int32.h>
-//#include <esp32-hal-ledc.h>
-#define USE_USBCON
+#include <esp32-hal-ledc.h> 
 
 ros::NodeHandle nh;
 
-int pwmChannel= 0;
+int pwmChannel = 0;
 int pwmPin = 2;
 int frequence = 1000; // 1KHz
 int resolution = 8;
 
-void vcc (const std_msgs::Int32 &PwmReceive){
+void vcc(const std_msgs::Int32 &PwmReceive) {
   int valorPwm = PwmReceive.data;
   ledcAttachPin(pwmPin, pwmChannel);
   ledcWrite(pwmChannel, valorPwm);
-  
 }
 
 ros::Subscriber<std_msgs::Int32> sub("PwmReceive", &vcc);
 
 void setup() {
   nh.getHardware()->setBaud(115200);
+
   ledcSetup(pwmChannel, frequence, resolution);
-  //delay(2000);
 
   nh.initNode();
   nh.subscribe(sub);
- 
- 
+
+  delay(200);
 }
 
 void loop() {
   nh.spinOnce();
-  //while(!nh.connected()) {nh.spinOnce();}
 }
