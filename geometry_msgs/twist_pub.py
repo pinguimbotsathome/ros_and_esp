ba5a10dab913482959653b349ac655d373f9b75f
Herick DallAgnol <140270456+HerickDallagnol@users.noreply.github.com>
2023-08-29 17:39:46 -0300

    Update twist_pub.py

--- geometry_msgs/twist_pub.py
+++ geometry_msgs/twist_pub.py
@@ -2,26 +2,26 @@
 import rospy
 from geometry_msgs.msg import Twist
 
+
 def pubTwist():
     rospy.init_node('pubTwist', anonymous=True)
 
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rate = rospy.Rate(1)  # 1 Hz
 
-    linearX = float(input("Linear X: "))
-    angularZ = float(input("Angular Z:"))
-
     twist =  Twist()
-    twist.linear.x = linearX
-    twist.angular.z = angularZ 
+    twist.linear.x = 0.5
+    twist.angular.z = 0
 
     while not rospy.is_shutdown():
         pub.publish(twist)  
-        rospy.loginfo("Linear X : %f  Angular Z: %f", twist.linear.x, twist.angular.z) 
+        rospy.loginfo("Linear: %f  Angular: %f", twist.linear.x, twist.angular.z) 
         rate.sleep()
+
 
 if __name__ == '__main__':
     try:
         pubTwist()
     except rospy.ROSInterruptException:
-        pass
+        pass    
+
